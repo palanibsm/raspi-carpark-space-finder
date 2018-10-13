57166acae5664ec4801bfe1e2394b4aef1f1b975
Senthil Baskaran <senthil2world@gmail.com>
2018-10-13 17:47:10 +1300

    Able to upload image file into S3

--- 1-push-image-to-s3/carpark-image-capture-upload.py
+++ 1-push-image-to-s3/carpark-image-capture-upload.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
-import sys, os, glob, time, boto3
+import sys, os, glob, time, boto3, urllib2, StringIO
+from boto3.s3.transfer import S3Transfer
 
+AWS_BUCKET = "senthil-raspi-carpark-space"
 session = boto3.Session(profile_name='senthil.ryde')
 s3client = session.resource('s3')
 s3putclient = session.client('s3')
-bucket = s3client.Bucket('senthil-raspi-carpark-space')
+bucket = s3client.Bucket(AWS_BUCKET)
 #print (bucket)
+sourceurl = 'https://images.freeimages.com/images/large-previews/bd7/falloxbow-1058032.jpg'
 
 directory = '/basinreserve/'
 
@@ -14,14 +17,23 @@ def list_bucket_contents(bucket):
       print (object.key)
 
 def put_object_into_s3(s3putclient):
+   
+   file_path = "abcd.txt" 
+   file_object = urllib2.urlopen(sourceurl)
+   fp = StringIO.StringIO(file_object.read())
+
    response = s3putclient.put_object(
       ACL='private',
-      Bucket='senthil-raspi-carpark-space',
-      Body='<h1>Hello Senthil</h1>',
-      Key='basin-reserve/index.html'
+      Bucket=AWS_BUCKET,
+      Body=fp,
+      Key='basin-reserve/test.jpg'
    )
-   
    print (response)
+
+   transfer = S3Transfer(s3putclient)
+   transfer.upload_file(file_path, AWS_BUCKET, file_path)
+   
+   #print (response)
 
  
 list_bucket_contents(bucket)
