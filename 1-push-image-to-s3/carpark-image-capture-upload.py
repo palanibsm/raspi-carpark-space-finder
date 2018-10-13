244e73967cfa94da03cc4b0cca8f926e9efb7548
Senthil Baskaran <senthil2world@gmail.com>
2018-10-13 17:15:13 +1300

    Ability to upload dummy file in AWS s3

--- 1-push-image-to-s3/carpark-image-capture-upload.py
+++ 1-push-image-to-s3/carpark-image-capture-upload.py
@@ -1,38 +1,28 @@
 #!/usr/bin/python
-# import sys, os, glob, time, boto
-# from boto.s3.connection import S3Connection
-# from boto.s3.key import Key
-import boto3
+import sys, os, glob, time, boto3
 
-print ('Senthil how are you...')
+session = boto3.Session(profile_name='senthil.ryde')
+s3client = session.resource('s3')
+s3putclient = session.client('s3')
+bucket = s3client.Bucket('senthil-raspi-carpark-space')
+#print (bucket)
 
-# dev = boto.session.Session(profile_name='senthil.ryde')
-s3 = boto3.resource('s3')
-# print (dev)
-# AWS_ACCESS =
-# AWS_SECRET =
-#
-# conn = S3Connection(AWS_ACCESS,AWS_SECRET)
-# bucket = conn.get_bucket('be.wapptastic')
-# directory = '/home/pi/Programs/'
-#
-# def percent_cb(complete, total):
-#     sys.stdout.write('.')
-#     sys.stdout.flush()
-#
-# def getFiles(dir):
-# 	return [os.path.basename(x) for x in glob.glob(str(dir) + '*.jpg')]
-#
-# def upload_S3(dir, file):
-# 	k = Key(bucket)
-# 	k.key = f
-# 	# k.set_contents_from_filename(dir + f, cb=percent_cb, num_cb=10)
-#
-#
-# # filenames = getFiles(directory)
-# # print filenames
-#
-# for f in filenames:
-#         print 'rnUploading %s to Amazon S3 bucket %s' % (f, bucket)
-# 	upload_S3(directory, f)
-#         removeLocal(directory, f)
+directory = '/basinreserve/'
+
+def list_bucket_contents(bucket):
+   for object in bucket.objects.all():
+      print (object.key)
+
+def put_object_into_s3(s3putclient):
+   response = s3putclient.put_object(
+      ACL='private',
+      Bucket='senthil-raspi-carpark-space',
+      Body='<h1>Hello Senthil</h1>',
+      Key='basin-reserve/index.html'
+   )
+   
+   print (response)
+
+ 
+list_bucket_contents(bucket)
+put_object_into_s3(s3putclient)
